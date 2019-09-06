2f62c90658839a0b1e0bd58b7f2acbb02e7e0476
Andrew Musgrave <andrew.musgrave@shopify.com>
2019-09-06 15:34:30 -0400

    Update core to log

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-core.log('i got ran');
-core.log(JSON.stringify(github.context));
+console.log('i got ran');
+console.log(JSON.stringify(github.context));
