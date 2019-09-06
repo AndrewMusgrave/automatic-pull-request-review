4d83e75f795e7a00fdc0fbb66fc62ec512455b97
Andrew Musgrave <andrew.musgrave@shopify.com>
2019-09-06 15:53:20 -0400

    Update

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const octokit = new github.GitHub(token);
+
 console.log('i got ran');
 console.log(JSON.stringify(github.context));
+
+
+(async() => {
+  await octokit.graphql(`mutation {
+    submitPullRequestReview( input: { clientMutationId: "${github.context.payload.base.repo.node_id}", pullRequestReview: "APPROVE"
+  }) { clientMutationId } }`)
+})
