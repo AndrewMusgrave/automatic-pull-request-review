0735164f2120d09d1cfa5218e5fca18ef61d10e0
Andrew Musgrave <andrew.musgrave@shopify.com>
2019-09-09 14:30:17 -0400

    Add log

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ const octokit = new github.GitHub(token);
 
 console.log(github.context.payload["pull_request"]);
 
-(async () => {
-  try {
-    await octokit.graphql(`
-      mutation {
-        submitPullRequestReview(input: {
-          pullRequestReviewId: "${
-            github.context.payload["pull_request"].base.repo["node_id"]
-          }",
-          event: APPROVE
-        }) {clientMutationId} }`);
-  } catch (e) {
-    console.error(e);
-  }
-})();
+// (async () => {
+//   try {
+//     await octokit.graphql(`
+//       mutation {
+//         submitPullRequestReview(input: {
+//           pullRequestReviewId: "${
+//             github.context.payload["pull_request"].base.repo["node_id"]
+//           }",
+//           event: APPROVE
+//         }) {clientMutationId} }`);
+//   } catch (e) {
+//     console.error(e);
+//   }
+// })();
