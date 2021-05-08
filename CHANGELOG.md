e2a515aa10a951082adf22422d54fdc3a977fc49
Alex Page <alex@alexpage.com.au>
2021-05-07 22:15:09 -0700

    Bump dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 
 ---
 
+## 0.0.4 - 2021-05-07
+
+- Update dependencies
+
 ## 0.0.3 - 2021-05-06
 
 - Add `repo-token` to actions.yml
--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+import core from '@actions/core';
+import github from '@actions/github';
 
 const token = core.getInput('repo-token');
 const requestEvent = core.getInput('event');
 const body = core.getInput('body');
 
-const octokit = new github.GitHub(token);
+const octokit = github.getOctokit(token)
 
 if (
   (requestEvent === 'COMMENT' || requestEvent === 'REQUEST_CHANGES') &&
@@ -20,17 +20,16 @@ if (!pullRequest) {
   core.setFailed('This action is meant to be ran on pull requests');
 }
 
-octokit
-  .graphql(
-    `
-      mutation {
-        addPullRequestReview(input: {
-          pullRequestId: "${(<any>pullRequest)['node_id']}",
-          event: ${requestEvent},
-          body: "${body}"
-        }) {clientMutationId} }`,
-  )
-  .catch((err) => {
-    core.error(err);
-    core.setFailed(err.message);
-  });
+const query = `
+mutation {
+  addPullRequestReview(input: {
+    pullRequestId: "${(<any>pullRequest)['node_id']}",
+    event: ${requestEvent},
+    body: "${body}"
+  }) {clientMutationId}
+}`;
+
+octokit.graphql(query).catch((err) => {
+  core.error(err);
+  core.setFailed(err.message);
+});
