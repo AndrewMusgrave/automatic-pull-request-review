7fe529c5b9820c7d97207a6640a69354000fa91d
Eduardo Fernandes <eduardo@patientsknowbest.com>
2022-02-07 18:13:02 +0100

    Automate approval for dependabot PRs

--- index.ts
+++ index.ts
@@ -1,16 +1,13 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
-const token = core.getInput('repo-token');
+const token = core.getInput('repo-token') || "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";
 const requestEvent = core.getInput('event');
 const body = core.getInput('body');
 
-const octokit = github.getOctokit(token)
+const octokit = github.getOctokit(token);
 
-if (
-  (requestEvent === 'COMMENT' || requestEvent === 'REQUEST_CHANGES') &&
-  !body
-) {
+if ((requestEvent === 'COMMENT' || requestEvent === 'REQUEST_CHANGES') && !body) {
   core.setFailed('Event type COMMENT & REQUEST_CHANGES require a body.');
 }
 
@@ -20,16 +17,26 @@ if (!pullRequest) {
   core.setFailed('This action is meant to be ran on pull requests');
 }
 
-const query = `
-mutation {
-  addPullRequestReview(input: {
-    pullRequestId: "${(<any>pullRequest)['node_id']}",
-    event: ${requestEvent},
-    body: "${body}"
-  }) {clientMutationId}
-}`;
+if (!pullRequest?.number) {
+  core.setFailed("Pull request number is unavailable");
+} else {
+  const pattern = /aws-java-sdk-s3 from (\d+)\.(\d+)\.\d+ to \1\.\2\.\d+/;
+  octokit.pulls.get({ owner: "patientsknowbest", repo: "phr", pull_number: pullRequest?.number })
+    .then(pull => {
+      if (pull.data.user?.login === "dependabot[bot]" && pattern.test(pull.data.title)) {
+        const query = `
+        mutation {
+          addPullRequestReview(input: {
+            pullRequestId: "${(<any>pullRequest)['node_id']}",
+            event: ${requestEvent},
+            body: "${body}"
+          }) {clientMutationId}
+        }`;
 
-octokit.graphql(query).catch((err) => {
-  core.error(err);
-  core.setFailed(err.message);
-});
+        octokit.graphql(query).catch((err) => {
+          core.error(err);
+          core.setFailed(err.message);
+        });
+      }
+    })
+}
